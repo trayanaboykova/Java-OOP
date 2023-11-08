7aa346e106e9357c71c8b2ac1210cfd3172dee8d
Trayana Boykova <trayana.boykova@gmail.com>
2023-11-08 16:35:56 +0200

    Update Main.java

--- src/Course01_WorkingWithAbstraction/Lab/P04_HotelReservation/Main.java
+++ src/Course01_WorkingWithAbstraction/Lab/P04_HotelReservation/Main.java
@@ -1,4 +1,21 @@
 package Course01_WorkingWithAbstraction.Lab.P04_HotelReservation;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] input = scanner.nextLine().split("\\s+");
+
+        double pricePerDay = Double.parseDouble(input[0]);
+        int numberOfDays = Integer.parseInt(input[1]);
+        Season season = Season.valueOf(input[2].toUpperCase());
+        DiscountType discountType = DiscountType.valueOf(input[3].toUpperCase());
+
+        double holidayPrice = PriceCalculator.calculateHolidayPrice(pricePerDay, numberOfDays, season, discountType);
+
+        System.out.printf("%.2f", holidayPrice);
+    }
 }
