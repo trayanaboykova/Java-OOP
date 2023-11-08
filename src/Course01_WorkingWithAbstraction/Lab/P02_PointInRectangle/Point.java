cb153e924388ddc3f3f39747d35d19afdf125fe5
Trayana Boykova <trayana.boykova@gmail.com>
2023-11-08 15:18:22 +0200

    Update Point.java

--- src/Course01_WorkingWithAbstraction/Lab/P02_PointInRectangle/Point.java
+++ src/Course01_WorkingWithAbstraction/Lab/P02_PointInRectangle/Point.java
@@ -1,4 +1,19 @@
 package Course01_WorkingWithAbstraction.Lab.P02_PointInRectangle;
 
 public class Point {
+    private final int x;
+    private final int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
 }
