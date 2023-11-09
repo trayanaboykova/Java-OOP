4f4a5796db5fa586663570ca87f13ea54e409ace
Trayana Boykova <trayana.boykova@gmail.com>
2023-11-09 13:26:16 +0200

    Create CardRanks.java

---
+++ src/Course01_WorkingWithAbstraction/Exercise/P02_CardRank/CardRanks.java
@@ -0,0 +1,17 @@
+package Course01_WorkingWithAbstraction.Exercise.P02_CardRank;
+
+public enum CardRanks {
+    ACE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING;
+}
