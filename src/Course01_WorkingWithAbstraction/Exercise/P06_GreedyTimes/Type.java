9efc0cf10a2c9f04f724c1c2986823dde35cff1c
Trayana Boykova <trayana.boykova@gmail.com>
2023-11-09 14:08:05 +0200

    Update Type.java

--- src/Course01_WorkingWithAbstraction/Exercise/P06_GreedyTimes/Type.java
+++ src/Course01_WorkingWithAbstraction/Exercise/P06_GreedyTimes/Type.java
@@ -1,4 +1,19 @@
 package Course01_WorkingWithAbstraction.Exercise.P06_GreedyTimes;
 
 public enum Type {
+    GOLD,
+    GEM,
+    CASH;
+
+    public static Type parse(String string) {
+        if (string.length() == 3) {
+            return Type.valueOf("CASH");
+        } else if (string.length() >= 4 && string.toLowerCase().endsWith("gem")) {
+            return Type.valueOf("GEM");
+        } else if (string.equalsIgnoreCase("Gold")) {
+            return Type.valueOf("GOLD");
+        } else {
+            throw new IllegalArgumentException("Unknown Type!");
+        }
+    }
 }
