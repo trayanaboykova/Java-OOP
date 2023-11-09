31c2e62b57567812a6aaab2bb4de3403700b4a0c
Trayana Boykova <trayana.boykova@gmail.com>
2023-11-09 13:59:58 +0200

    Update Galaxy.java

--- src/Course01_WorkingWithAbstraction/Exercise/P05_JediGalaxy/Galaxy.java
+++ src/Course01_WorkingWithAbstraction/Exercise/P05_JediGalaxy/Galaxy.java
@@ -1,4 +1,21 @@
 package Course01_WorkingWithAbstraction.Exercise.P05_JediGalaxy;
 
 public class Galaxy {
+    private Field field;
+    private Evil evil;
+    private Player player;
+
+    public Galaxy(Field field, Evil evil, Player player) {
+        this.field = field;
+        this.evil = evil;
+        this.player = player;
+    }
+
+    public void moveEvil() {
+        evil.move(this.field);
+    }
+
+    public void movePlayer() {
+        player.move(field);
+    }
 }
