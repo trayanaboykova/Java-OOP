6ec05312667f061c7b3d3cf40cba269769838dcf
Trayana Boykova <trayana.boykova@gmail.com>
2023-11-14 17:17:46 +0200

    Update Main.java

--- src/Course02_Encapsulation/Lab/P01_SortByNameAndAge/Main.java
+++ src/Course02_Encapsulation/Lab/P01_SortByNameAndAge/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
