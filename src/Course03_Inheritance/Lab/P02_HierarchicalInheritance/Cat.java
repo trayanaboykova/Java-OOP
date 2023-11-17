6e2c9c9fbb7e3215371bdf9f774cea182982f169
Trayana Boykova <trayana.boykova@gmail.com>
2023-11-17 15:25:00 +0200

    Update Cat.java

--- src/Course03_Inheritance/Lab/P02_HierarchicalInheritance/Cat.java
+++ src/Course03_Inheritance/Lab/P02_HierarchicalInheritance/Cat.java
@@ -1,6 +1,6 @@
 package Course03_Inheritance.Lab.P02_HierarchicalInheritance;
 
-public class Cat {
+public class Cat extends Animal {
     public void meow() {
         System.out.println("meowing...");
     }
