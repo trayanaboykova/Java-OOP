a515f9c08b3851b9c466baa95fa335dafb455b98
Trayana Boykova <trayana.boykova@gmail.com>
2023-11-17 15:42:19 +0200

    Update Main.java

--- src/Course03_Inheritance/Exercise/P02_Zoo/Main.java
+++ src/Course03_Inheritance/Exercise/P02_Zoo/Main.java
@@ -1,4 +1,9 @@
 package Course03_Inheritance.Exercise.P02_Zoo;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
 }
