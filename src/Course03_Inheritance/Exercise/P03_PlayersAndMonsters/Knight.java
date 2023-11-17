d592c346e1913c5a43606bdbffbcfb4ea00c54a9
Trayana Boykova <trayana.boykova@gmail.com>
2023-11-17 15:55:18 +0200

    Create Knight.java

---
+++ src/Course03_Inheritance/Exercise/P03_PlayersAndMonsters/Knight.java
@@ -0,0 +1,8 @@
+package Course03_Inheritance.Exercise.P03_PlayersAndMonsters;
+
+public class Knight  extends Hero {
+
+    public Knight(String username, int level) {
+        super(username, level);
+    }
+}
