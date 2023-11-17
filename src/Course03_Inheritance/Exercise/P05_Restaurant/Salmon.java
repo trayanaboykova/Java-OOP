40f04e4b1e3db1fd9b95dd17ab059928047f0ce1
Trayana Boykova <trayana.boykova@gmail.com>
2023-11-17 16:20:18 +0200

    Update Salmon.java

--- src/Course03_Inheritance/Exercise/P05_Restaurant/Salmon.java
+++ src/Course03_Inheritance/Exercise/P05_Restaurant/Salmon.java
@@ -1,4 +1,12 @@
 package Course03_Inheritance.Exercise.P05_Restaurant;
 
-public class Salmon {
+import java.math.BigDecimal;
+
+public class Salmon extends MainDish {
+
+    private final static double SALMON_GRAMS = 22;
+
+    public Salmon(String name, BigDecimal price) {
+        super(name, price, SALMON_GRAMS);
+    }
 }
