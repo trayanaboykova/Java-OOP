1784365286cc891f0db59e9c2eefa2622f73896a
Trayana Boykova <trayana.boykova@gmail.com>
2023-11-17 16:20:31 +0200

    Update Tea.java

--- src/Course03_Inheritance/Exercise/P05_Restaurant/Tea.java
+++ src/Course03_Inheritance/Exercise/P05_Restaurant/Tea.java
@@ -1,4 +1,10 @@
 package Course03_Inheritance.Exercise.P05_Restaurant;
 
-public class Tea {
+import java.math.BigDecimal;
+
+public class Tea extends HotBeverage {
+
+    public Tea(String name, BigDecimal price, double milliliters) {
+        super(name, price, milliliters);
+    }
 }
