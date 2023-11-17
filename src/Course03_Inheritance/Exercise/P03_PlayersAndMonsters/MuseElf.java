83af414968489c57326778e9268eb7241116dd2e
Trayana Boykova <trayana.boykova@gmail.com>
2023-11-17 15:55:21 +0200

    Create MuseElf.java

---
+++ src/Course03_Inheritance/Exercise/P03_PlayersAndMonsters/MuseElf.java
@@ -0,0 +1,8 @@
+package Course03_Inheritance.Exercise.P03_PlayersAndMonsters;
+
+public class MuseElf extends Elf {
+
+    public MuseElf(String username, int level) {
+        super(username, level);
+    }
+}
