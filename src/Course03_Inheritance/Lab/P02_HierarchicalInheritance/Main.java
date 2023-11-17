d1875ab05af32ccb2e983faa6762098c75ccdd79
Trayana Boykova <trayana.boykova@gmail.com>
2023-11-17 15:23:28 +0200

    Update Main.java

--- src/Course03_Inheritance/Lab/P02_HierarchicalInheritance/Main.java
+++ src/Course03_Inheritance/Lab/P02_HierarchicalInheritance/Main.java
@@ -1,5 +1,14 @@
 package Course03_Inheritance.Lab.P02_HierarchicalInheritance;
 
 public class Main {
-    
+    public static void main(String[] args) {
+
+        Dog dog = new Dog();
+        dog.eat();
+        dog.bark();
+
+        Cat cat = new Cat();
+        cat.eat();
+        cat.meow();
+    }
 }
