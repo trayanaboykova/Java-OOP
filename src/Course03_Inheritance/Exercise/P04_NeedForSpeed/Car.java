2d3ddf6c6fd995c8eaa2fc8f54b75eda0a23b063
Trayana Boykova <trayana.boykova@gmail.com>
2023-11-17 15:59:52 +0200

    Update Car.java

--- src/Course03_Inheritance/Exercise/P04_NeedForSpeed/Car.java
+++ src/Course03_Inheritance/Exercise/P04_NeedForSpeed/Car.java
@@ -1,4 +1,11 @@
 package Course03_Inheritance.Exercise.P04_NeedForSpeed;
 
-public class Car {
+public class Car extends Vehicle {
+
+    private final static double DEFAULT_FUEL_CONSUMPTION = 3;
+
+    public Car(double fuel, int horsePower) {
+        super(fuel, horsePower);
+        super.setFuelConsumption(DEFAULT_FUEL_CONSUMPTION);
+    }
 }
