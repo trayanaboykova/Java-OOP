4287839eebec761af8cb09493f0a3e7d75b325ec
Trayana Boykova <trayana.boykova@gmail.com>
2023-11-17 16:20:24 +0200

    Update Soup.java

--- src/Course03_Inheritance/Exercise/P05_Restaurant/Soup.java
+++ src/Course03_Inheritance/Exercise/P05_Restaurant/Soup.java
@@ -1,4 +1,10 @@
 package Course03_Inheritance.Exercise.P05_Restaurant;
 
-public class Soup {
+import java.math.BigDecimal;
+
+public class Soup extends Starter {
+
+    public Soup(String name, BigDecimal price, double grams) {
+        super(name, price, grams);
+    }
 }
