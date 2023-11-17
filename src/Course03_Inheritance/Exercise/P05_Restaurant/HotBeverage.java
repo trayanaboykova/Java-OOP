03965a8666ab11ce3a1b0862ccc4abd004ac5554
Trayana Boykova <trayana.boykova@gmail.com>
2023-11-17 16:19:53 +0200

    Update HotBeverage.java

--- src/Course03_Inheritance/Exercise/P05_Restaurant/HotBeverage.java
+++ src/Course03_Inheritance/Exercise/P05_Restaurant/HotBeverage.java
@@ -1,4 +1,10 @@
 package Course03_Inheritance.Exercise.P05_Restaurant;
 
-public class HotBeverage {
+import java.math.BigDecimal;
+
+public class HotBeverage  extends Beverage {
+
+    public HotBeverage(String name, BigDecimal price, double milliliters) {
+        super(name, price, milliliters);
+    }
 }
