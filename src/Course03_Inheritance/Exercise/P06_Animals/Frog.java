799e469429b03df076ffbc6267acb000e8f5250c
Trayana Boykova <trayana.boykova@gmail.com>
2023-11-17 16:25:00 +0200

    Update Frog.java

--- src/Course03_Inheritance/Exercise/P06_Animals/Frog.java
+++ src/Course03_Inheritance/Exercise/P06_Animals/Frog.java
@@ -1,4 +1,13 @@
 package Course03_Inheritance.Exercise.P06_Animals;
 
-public class Frog {
+public class Frog extends Animal {
+
+    public Frog(String name, int age, String gender) {
+        super(name, age, gender);
+    }
+
+    @Override
+    public String produceSound() {
+        return "Ribbit";
+    }
 }
