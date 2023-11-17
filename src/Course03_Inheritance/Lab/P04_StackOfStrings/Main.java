6e1573f0401e558f7e3fb0e75fe1e8eee5c5d362
Trayana Boykova <trayana.boykova@gmail.com>
2023-11-17 15:28:58 +0200

    Update Main.java

--- src/Course03_Inheritance/Lab/P04_StackOfStrings/Main.java
+++ src/Course03_Inheritance/Lab/P04_StackOfStrings/Main.java
@@ -1,4 +1,19 @@
 package Course03_Inheritance.Lab.P04_StackOfStrings;
 
 public class Main {
+    public static void main(String[] args) {
+
+        StackOfStrings sos = new StackOfStrings();
+
+        sos.push("one");
+        sos.push("two");
+        sos.push("three");
+
+        System.out.println(sos.isEmpty());
+        System.out.println(sos.peek());
+
+        System.out.println(sos.pop());
+        System.out.println(sos.pop());
+        System.out.println(sos.pop());
+    }
 }
