f984883ab85b5f680014d8259d17b09d7b3437e7
Trayana Boykova <trayana.boykova@gmail.com>
2023-11-17 15:25:03 +0200

    Update Dog.java

--- src/Course03_Inheritance/Lab/P02_HierarchicalInheritance/Dog.java
+++ src/Course03_Inheritance/Lab/P02_HierarchicalInheritance/Dog.java
@@ -1,4 +1,7 @@
 package Course03_Inheritance.Lab.P02_HierarchicalInheritance;
 
-public class Dog {
+public class Dog extends Animal{
+    public void bark() {
+        System.out.println("barking...");
+    }
 }
