aa0bec5109e6aabf0d7be54b16826e697aa17d81
Trayana Boykova <trayana.boykova@gmail.com>
2023-11-17 15:55:05 +0200

    Create BladeKnight.java

---
+++ src/Course03_Inheritance/Exercise/P03_PlayersAndMonsters/BladeKnight.java
@@ -0,0 +1,8 @@
+package Course03_Inheritance.Exercise.P03_PlayersAndMonsters;
+
+public class BladeKnight extends DarkKnight {
+
+    public BladeKnight(String username, int level) {
+        super(username, level);
+    }
+}
