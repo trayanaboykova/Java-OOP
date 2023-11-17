ce73c0936881ea5661f8e28031f1859575898163
Trayana Boykova <trayana.boykova@gmail.com>
2023-11-17 15:59:57 +0200

    Update FamilyCar.java

--- src/Course03_Inheritance/Exercise/P04_NeedForSpeed/FamilyCar.java
+++ src/Course03_Inheritance/Exercise/P04_NeedForSpeed/FamilyCar.java
@@ -1,4 +1,8 @@
 package Course03_Inheritance.Exercise.P04_NeedForSpeed;
 
-public class FamilyCar {
+public class FamilyCar extends Car {
+
+    public FamilyCar(double fuel, int horsePower) {
+        super(fuel, horsePower);
+    }
 }
