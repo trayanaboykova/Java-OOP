269f4444dda66babfcfbf720c25a4b3c21848817
Trayana Boykova <trayana.boykova@gmail.com>
2023-11-24 14:33:58 +0200

    Update Commando.java

--- src/Course04_InterfacedAndAbstraction/Exercise/P06_MilitaryElite/Commando.java
+++ src/Course04_InterfacedAndAbstraction/Exercise/P06_MilitaryElite/Commando.java
@@ -1,4 +1,10 @@
 package Course04_InterfacedAndAbstraction.Exercise.P06_MilitaryElite;
 
-public class Commando {
+import java.util.Collection;
+
+public interface Commando extends SpecialisedSoldier {
+
+    Collection<MissionImpl> getMission();
+
+    void addMission(MissionImpl mission);
 }
