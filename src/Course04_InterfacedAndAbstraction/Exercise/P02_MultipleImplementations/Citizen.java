19d52d51267687b9dd7cc9356801132e69649106
Trayana Boykova <trayana.boykova@gmail.com>
2023-11-24 14:06:36 +0200

    Create Citizen.java

---
+++ src/Course04_InterfacedAndAbstraction/Exercise/P02_MultipleImplementations/Citizen.java
@@ -0,0 +1,41 @@
+package Course04_InterfacedAndAbstraction.Exercise.P02_MultipleImplementations;
+
+public class Citizen implements Person, Identifiable, Birthable {
+
+    private String name;
+    private int age;
+    private String id;
+    private String birthDate;
+
+    public Citizen(String name, int age, String id, String birthDate) {
+        this.name = name;
+        this.age = age;
+        this.id = id;
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
