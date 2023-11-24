6756f4b02201724604d1f84995fa925ecf22541f
Trayana Boykova <trayana.boykova@gmail.com>
2023-11-24 14:40:37 +0200

    Update AddCollection.java

--- src/Course04_InterfacedAndAbstraction/Exercise/P07_CollectionHierarchy/AddCollection.java
+++ src/Course04_InterfacedAndAbstraction/Exercise/P07_CollectionHierarchy/AddCollection.java
@@ -1,4 +1,11 @@
 package Course04_InterfacedAndAbstraction.Exercise.P07_CollectionHierarchy;
 
-public class AddCollection {
+public class AddCollection extends Collection implements Addable {
+    @Override
+    public int add(String item) {
+
+        super.items.add(item);
+
+        return super.items.indexOf(item);
+    }
 }
