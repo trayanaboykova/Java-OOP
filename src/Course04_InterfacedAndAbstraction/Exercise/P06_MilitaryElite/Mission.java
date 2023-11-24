e5eda22f65fc43727bccd26358d6915e15675439
Trayana Boykova <trayana.boykova@gmail.com>
2023-11-24 14:34:17 +0200

    Update Mission.java

--- src/Course04_InterfacedAndAbstraction/Exercise/P06_MilitaryElite/Mission.java
+++ src/Course04_InterfacedAndAbstraction/Exercise/P06_MilitaryElite/Mission.java
@@ -1,4 +1,7 @@
 package Course04_InterfacedAndAbstraction.Exercise.P06_MilitaryElite;
 
-public class Mission {
+public interface Mission {
+    String getMissionCodeName();
+
+    State getState();
 }
