54c72d176e1a210c7710e7b9a4148e11f8ef6f75
Trayana Boykova <trayana.boykova@gmail.com>
2023-11-24 14:34:40 +0200

    Update SoldierImpl.java

--- src/Course04_InterfacedAndAbstraction/Exercise/P06_MilitaryElite/SoldierImpl.java
+++ src/Course04_InterfacedAndAbstraction/Exercise/P06_MilitaryElite/SoldierImpl.java
@@ -1,4 +1,33 @@
 package Course04_InterfacedAndAbstraction.Exercise.P06_MilitaryElite;
 
-public class SoldierImpl {
+public abstract class SoldierImpl implements Soldier {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public SoldierImpl(int id, String firstName, String lastName) {
+
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public int getId() {
+
+        return id;
+    }
+
+    @Override
+    public String getFirstName() {
+
+        return firstName;
+    }
+
+    @Override
+    public String getLastName() {
+
+        return lastName;
+    }
 }
