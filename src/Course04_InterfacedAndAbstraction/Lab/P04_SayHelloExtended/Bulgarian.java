d58fb68e058ad97e39d5aa4aa8a76b320e678062
Trayana Boykova <trayana.boykova@gmail.com>
2023-11-24 12:14:21 +0200

    Update Bulgarian.java

--- src/Course04_InterfacedAndAbstraction/Lab/P04_SayHelloExtended/Bulgarian.java
+++ src/Course04_InterfacedAndAbstraction/Lab/P04_SayHelloExtended/Bulgarian.java
@@ -1,4 +1,12 @@
 package Course04_InterfacedAndAbstraction.Lab.P04_SayHelloExtended;
 
-public class Bulgarian {
+public class Bulgarian extends BasePerson{
+    public Bulgarian(String name) {
+        super(name);
+    }
+
+    @Override
+    public String sayHello() {
+        return "Здравей";
+    }
 }
