8079d5f7feceb912f926f2b6550c8c064154b8c7
Trayana Boykova <trayana.boykova@gmail.com>
2023-11-24 14:34:50 +0200

    Update SpyImpl.java

--- src/Course04_InterfacedAndAbstraction/Exercise/P06_MilitaryElite/SpyImpl.java
+++ src/Course04_InterfacedAndAbstraction/Exercise/P06_MilitaryElite/SpyImpl.java
@@ -1,4 +1,30 @@
 package Course04_InterfacedAndAbstraction.Exercise.P06_MilitaryElite;
 
-public class SpyImpl {
+public class SpyImpl extends SoldierImpl implements Spy {
+
+    private String codeNumber;
+
+    public SpyImpl(int id, String firstName, String lastName, String codeNumber) {
+
+        super(id, firstName, lastName);
+        this.codeNumber = codeNumber;
+    }
+
+    @Override
+    public String getCodeNumber() {
+
+        return codeNumber;
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(String.format("Name: %s %s Id: %d", getFirstName(), getLastName(), getId()))
+                .append(System.lineSeparator())
+                .append(String.format("Code Number: %s", codeNumber));
+
+        return sb.toString().trim();
+    }
 }
