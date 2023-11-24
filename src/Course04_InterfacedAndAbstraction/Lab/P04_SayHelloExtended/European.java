3ee257dab997484f967aa2cd4d334b43c26ef66f
Trayana Boykova <trayana.boykova@gmail.com>
2023-11-24 12:14:29 +0200

    Update European.java

--- src/Course04_InterfacedAndAbstraction/Lab/P04_SayHelloExtended/European.java
+++ src/Course04_InterfacedAndAbstraction/Lab/P04_SayHelloExtended/European.java
@@ -1,4 +1,11 @@
 package Course04_InterfacedAndAbstraction.Lab.P04_SayHelloExtended;
 
-public class European {
+public class European extends BasePerson {
+    public European(String name) {
+        super(name);
+    }
+    @Override
+    public String sayHello() {
+        return "Hello";
+    }
 }
