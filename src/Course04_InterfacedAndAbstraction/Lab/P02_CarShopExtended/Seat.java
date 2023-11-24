10d7323471560cee5d15fe19a6d92f64eea9bb28
Trayana Boykova <trayana.boykova@gmail.com>
2023-11-24 12:04:25 +0200

    Update Seat.java

--- src/Course04_InterfacedAndAbstraction/Lab/P02_CarShopExtended/Seat.java
+++ src/Course04_InterfacedAndAbstraction/Lab/P02_CarShopExtended/Seat.java
@@ -1,4 +1,28 @@
 package Course04_InterfacedAndAbstraction.Lab.P02_CarShopExtended;
 
-public class Seat {
+public class Seat extends CarImpl implements Sellable {
+
+    private Double price;
+
+    public Seat(String model, String color, Integer horsePower, String countryProduced, Double price) {
+        super(model, color, horsePower, countryProduced);
+        this.price = price;
+    }
+
+    @Override
+    public Double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(super.toString())
+                .append(System.lineSeparator())
+                .append(String.format("%s sells for %.6f", getModel(), getPrice()));
+
+        return sb.toString();
+    }
 }
