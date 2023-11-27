e07ed58a9207f5ceab64a6858ce5ebd3d06552a4
Trayana Boykova <trayana.boykova@gmail.com>
2023-11-27 18:59:47 +0200

    Create Felime.java

---
+++ src/Course05_Polymorphism/Lab/P04_WildFarm/Felime.java
@@ -0,0 +1,7 @@
+package Course05_Polymorphism.Lab.P04_WildFarm;
+
+public abstract class Felime extends Mammal {
+    protected Felime(String animalName, String animalType, Double animalWeight, String livingRegion) {
+        super(animalName, animalType, animalWeight, livingRegion);
+    }
+}
