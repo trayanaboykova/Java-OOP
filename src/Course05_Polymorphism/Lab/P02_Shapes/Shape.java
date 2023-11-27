5228a08d5f211b868bb793531a9c2f33d7795b50
Trayana Boykova <trayana.boykova@gmail.com>
2023-11-27 18:44:24 +0200

    Create Shape.java

---
+++ src/Course05_Polymorphism/Lab/P02_Shapes/Shape.java
@@ -0,0 +1,22 @@
+package Course05_Polymorphism.Lab.P02_Shapes;
+
+public abstract class Shape {
+    private Double perimeter;
+    private Double area;
+
+
+
+    public Double getPerimeter() {
+        return perimeter;
+    }
+
+    public Double getArea() {
+        return area;
+    }
+
+    public abstract Double calculatePerimeter();
+
+    public abstract Double calculateArea();
+
+
+}
