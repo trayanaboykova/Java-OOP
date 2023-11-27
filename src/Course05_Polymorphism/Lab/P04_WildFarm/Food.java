a361cb777a374ac3d513abf7f37ba9f20eca5c76
Trayana Boykova <trayana.boykova@gmail.com>
2023-11-27 18:59:59 +0200

    Create Food.java

---
+++ src/Course05_Polymorphism/Lab/P04_WildFarm/Food.java
@@ -0,0 +1,17 @@
+package Course05_Polymorphism.Lab.P04_WildFarm;
+
+public abstract class Food {
+    protected Integer quantity;
+
+    public Food(Integer quantity) {
+        setQuantity(quantity);
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
