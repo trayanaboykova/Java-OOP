766071a4ac69f26799abe8776e928ed397e8eb72
Trayana Boykova <trayana.boykova@gmail.com>
2023-11-27 18:59:51 +0200

    Create Zebra.java

---
+++ src/Course05_Polymorphism/Lab/P04_WildFarm/Zebra.java
@@ -0,0 +1,27 @@
+package Course05_Polymorphism.Lab.P04_WildFarm;
+
+public class Zebra extends Mammal{
+    protected Zebra(String animalName, String animalType, Double animalWeight, String livingRegion) {
+        super(animalName, animalType, animalWeight, livingRegion);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Zs");
+    }
+
+    @Override
+    public void eat(Food food) {
+
+        if (food instanceof Vegetable) {
+            setFoodEaten(food.getQuantity() + getFoodEaten());
+        } else {
+            System.out.println("Zebras are not eating that type of food!");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
