9192afbb43556266f1a551baa4a5e8c016cc3f7c
Trayana Boykova <trayana.boykova@gmail.com>
2023-11-28 17:09:29 +0200

    Create PasteTransform.java

---
+++ src/Course05_Polymorphism/Exercise/P03_Word/PasteTransform.java
@@ -0,0 +1,15 @@
+package Course05_Polymorphism.Exercise.P03_Word;
+
+public class PasteTransform implements TextTransform {
+
+    private CutTransform cutTransform;
+
+    public PasteTransform(CutTransform cutTransform) {
+        this.cutTransform = cutTransform;
+    }
+
+    @Override
+    public void invokeOn(StringBuilder text, int startIndex, int endIndex) {
+        text.replace(startIndex, endIndex, String.valueOf(cutTransform.getCutText()));
+    }
+}
