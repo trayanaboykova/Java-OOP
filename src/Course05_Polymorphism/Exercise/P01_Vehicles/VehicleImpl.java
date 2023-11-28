1ba1b74033abafbee5bea662e2a620721389e497
Trayana Boykova <trayana.boykova@gmail.com>
2023-11-28 15:31:43 +0200

    Create VehicleImpl.java

---
+++ src/Course05_Polymorphism/Exercise/P01_Vehicles/VehicleImpl.java
@@ -0,0 +1,28 @@
+package Course05_Polymorphism.Exercise.P01_Vehicles;
+
+public abstract class VehicleImpl implements Vehicle {
+
+    private double fuelQuantity;
+    private double litersPerKm;
+
+    public VehicleImpl(double fuelQuantity, double litersPerKm) {
+        setFuelQuantity(fuelQuantity);
+        setLitersPerKm(litersPerKm);
+    }
+
+    protected double getFuelQuantity() {
+        return fuelQuantity;
+    }
+
+    protected void setFuelQuantity(double fuelQuantity) {
+        this.fuelQuantity = fuelQuantity;
+    }
+
+    protected double getLitersPerKm() {
+        return litersPerKm;
+    }
+
+    protected void setLitersPerKm(double litersPerKm) {
+        this.litersPerKm = litersPerKm;
+    }
+}
