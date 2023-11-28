603e743c843d43e84c284a192b3dd5a5bbf90ce4
Trayana Boykova <trayana.boykova@gmail.com>
2023-11-28 16:21:04 +0200

    Create FuelModifier.java

---
+++ src/Course05_Polymorphism/Exercise/P02_VehiclesExtension/FuelModifier.java
@@ -0,0 +1,19 @@
+package Course05_Polymorphism.Exercise.P02_VehiclesExtension;
+
+public enum FuelModifier  {
+
+    CAR_AC_ADDITIONAL_CONSUMPTION(0.9),
+    TRUCK_AC_ADDITIONAL_CONSUMPTION(1.6),
+    TRUCK_TANK_FUEL_DEDUCTION(0.95),
+    BUS_WITH_PASSENGERS_ADDITIONAL_CONSUMPTION(1.4);
+
+    private double fuelModifier;
+
+    FuelModifier(double fuelModifier) {
+        this.fuelModifier = fuelModifier;
+    }
+
+    public double getFuelModifier() {
+        return fuelModifier;
+    }
+}
